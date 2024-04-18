dac7e3bcc8aa452cd43b6b75ae6746ea2c50329c
adrwlo <adrwlo@o2.pl>
2024-04-19 00:23:44 +0200

    quiz some changes

--- src/main/java/com/example/quizbackend/controller/QuizController.java
+++ src/main/java/com/example/quizbackend/controller/QuizController.java
@@ -3,6 +3,7 @@ package com.example.quizbackend.controller;
 import com.example.quizbackend.entity.QuizDataDTO;
 import com.example.quizbackend.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +22,15 @@ public class QuizController {
     }
 
     @PostMapping("/addQuiz")
-    public Void addQuiz(@RequestBody QuizDataDTO quizDataDTO) {
-        quizService.addQuiz(quizDataDTO);
-        return null;
+    public ResponseEntity<String> addQuiz(@RequestBody QuizDataDTO quizDataDTO) {
+        try {
+            quizService.addQuiz(quizDataDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while adding the quiz.");
+        }
     }
 
     @DeleteMapping("deleteQuiz/{title}")
